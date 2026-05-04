54bb5a4485bec25bdaeacbd2a0094b19fc513823
Suraj Chaurasia <suraj84chaurasia@gmail.com>
2026-05-04 22:24:05 +0530

    feat: improve Add Habit modal UI and functionality

--- lib/widgets/add_habit_modal.dart
+++ lib/widgets/add_habit_modal.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/habit_provider.dart';
 import '../theme/app_theme.dart';
+import 'dart:math';
 
 class AddHabitModal extends StatefulWidget {
   const AddHabitModal({super.key});
@@ -10,11 +11,41 @@ class AddHabitModal extends StatefulWidget {
   State<AddHabitModal> createState() => _AddHabitModalState();
 }
 
-class _AddHabitModalState extends State<AddHabitModal> {
+class _AddHabitModalState extends State<AddHabitModal> with SingleTickerProviderStateMixin {
   final _nameController = TextEditingController();
   final _descController = TextEditingController();
   String _selectedIcon = 'star';
-  Color _selectedColor = AppTheme.primaryColor;
+  Color _selectedColor = Colors.black;
+  bool _hasError = false;
+  
+  late AnimationController _shakeController;
+  late Animation<double> _shakeAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _shakeController = AnimationController(
+      duration: const Duration(milliseconds: 300),
+      vsync: this,
+    );
+
+    _shakeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(_shakeController);
+  }
+
+  @override
+  void dispose() {
+    _shakeController.dispose();
+    _nameController.dispose();
+    _descController.dispose();
+    super.dispose();
+  }
+
+  void _shake() {
+    _shakeController.forward(from: 0.0);
+    setState(() {
+      _hasError = true;
+    });
+  }
 
   final List<String> _icons = [
     'star',
@@ -27,31 +58,54 @@ class _AddHabitModalState extends State<AddHabitModal> {
     'restaurant',
     'code',
     'payments',
+    'pool',
+    'pedal_bike',
+    'brush',
+    'language',
+    'spa',
+    'work',
+    'pets',
+    'sunny',
+    'music_note',
+    'hiking',
   ];
 
   final List<Color> _colors = [
-    AppTheme.primaryColor,
+    Colors.black,
     const Color(0xFF39D2C0),
     const Color(0xFFEE8B60),
     const Color(0xFF24D1E3),
-    const Color(0xFF616161),
-    const Color(0xFFD9D9D9),
     const Color(0xFF4B39EF),
     const Color(0xFFFF5963),
+    const Color(0xFF9C27B0), // Purple
+    const Color(0xFF4CAF50), // Green
+    const Color(0xFFFFC107), // Amber
+    const Color(0xFF3F51B5), // Indigo
+    const Color(0xFFE91E63), // Pink
   ];
 
   IconData _getIconData(String name) {
     switch (name) {
-      case 'directions_run': return Icons.directions_run;
-      case 'local_drink': return Icons.local_drink;
-      case 'menu_book': return Icons.menu_book;
-      case 'fitness_center': return Icons.fitness_center;
-      case 'self_improvement': return Icons.self_improvement;
-      case 'nightlight_round': return Icons.nightlight_round;
-      case 'restaurant': return Icons.restaurant;
-      case 'code': return Icons.code;
-      case 'payments': return Icons.payments;
-      default: return Icons.star;
+      case 'directions_run': return Icons.directions_run_rounded;
+      case 'local_drink': return Icons.local_drink_rounded;
+      case 'menu_book': return Icons.menu_book_rounded;
+      case 'fitness_center': return Icons.fitness_center_rounded;
+      case 'self_improvement': return Icons.self_improvement_rounded;
+      case 'nightlight_round': return Icons.nightlight_round_rounded;
+      case 'restaurant': return Icons.restaurant_rounded;
+      case 'code': return Icons.code_rounded;
+      case 'payments': return Icons.payments_rounded;
+      case 'pool': return Icons.pool_rounded;
+      case 'pedal_bike': return Icons.pedal_bike_rounded;
+      case 'brush': return Icons.brush_rounded;
+      case 'language': return Icons.language_rounded;
+      case 'spa': return Icons.spa_rounded;
+      case 'work': return Icons.work_rounded;
+      case 'pets': return Icons.pets_rounded;
+      case 'sunny': return Icons.sunny;
+      case 'music_note': return Icons.music_note_rounded;
+      case 'hiking': return Icons.hiking_rounded;
+      default: return Icons.star_rounded;
     }
   }
 
@@ -82,12 +136,47 @@ class _AddHabitModalState extends State<AddHabitModal> {
             ],
           ),
           const SizedBox(height: 20),
-          TextField(
-            controller: _nameController,
-            decoration: InputDecoration(
-              labelText: "Habit Name",
-              hintText: "e.g. Read for 20 mins",
-              border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+          AnimatedBuilder(
+            animation: _shakeAnimation,
+            builder: (context, child) {
+              final double offset = sin(_shakeAnimation.value * pi * 4) * 8;
+              return Transform.translate(
+                offset: Offset(offset, 0),
+                child: child,
+              );
+            },
+            child: TextField(
+              controller: _nameController,
+              textCapitalization: TextCapitalization.sentences,
+              decoration: InputDecoration(
+                labelText: "Habit Name",
+                hintText: "e.g. Read for 20 mins",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  borderSide: BorderSide(
+                    color: _hasError ? Colors.red : Colors.grey.shade300,
+                    width: _hasError ? 2 : 1,
+                  ),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  borderSide: BorderSide(
+                    color: _hasError ? Colors.red : Colors.grey.shade300,
+                    width: _hasError ? 2 : 1,
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  borderSide: BorderSide(
+                    color: _hasError ? Colors.red : AppTheme.primaryColor,
+                    width: 2,
+                  ),
+                ),
+                labelStyle: TextStyle(color: _hasError ? Colors.red : null),
+              ),
+              onChanged: (value) {
+                if (_hasError) setState(() => _hasError = false);
+              },
             ),
           ),
           const SizedBox(height: 16),
@@ -175,6 +264,8 @@ class _AddHabitModalState extends State<AddHabitModal> {
                     _selectedColor.value,
                   );
                   Navigator.pop(context);
+                } else {
+                  _shake();
                 }
               },
               style: ElevatedButton.styleFrom(
